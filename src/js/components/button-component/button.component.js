3ab01adb06d1315f7af285c990760a782d6562a4
Jose Osorio <joseosorio001@outlook.com>
2018-04-08 22:43:13 -0600

    prueba

--- src/js/components/button-component/button.component.js
+++ src/js/components/button-component/button.component.js
@@ -1,3 +1,5 @@
+import PELICULAS from './mock-peliculas.js';
+
 class CustomButton extends HTMLElement {
   constructor() {
     super();//llamamos al constructor de HTMLElement
@@ -7,15 +9,20 @@ class CustomButton extends HTMLElement {
   //Se construirá un bortón y un párrafo
   //Si el botón es presionado desaparece o aparece el texto del párrafo
   connectedCallback() {
+    let pelis = new Object(PELICULAS);
     let estado = true;//maneja el estado de visible/no visible
     //creación de los elementos del componente
+    let t = document.createTextNode(() => for (let p of pelis) {
+      return `${p.year} - ${p.name}`;
+    });
     const customButton = document.createElement('button');
     const par = document.createElement('p');
+    par.appendChild(t);
     //agregar los elementos al DOM
     document.body.appendChild(customButton);
     document.body.appendChild(par);
 
-    par.setAttribute("id", "p");//agrega el atributo id="p"
+    par.setAttribute('id', 'par');
     customButton.innerText = "Desaparecer";
     //se agrega un Listener al botón con la funcion para aparecer y desaparecer
     customButton.addEventListener('click', function() {
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,1 @@
 import CustomButton from './components/button-component/button.component.js';
-import { PELICULAS } from './mock-peliculas.js';
-
-let pelis = new Object(PELICULAS);
-
-console.log(pelis);
-let p = document.getElementById('p');
-p.innerText = pelis[0].name + "\n" + pelis[1].name;
